33375124f0d538e8fdd99bb2ada04366f7cb511a
USUARIO <USUARIO@DESKTOP-6O7JB34>
2023-08-09 20:34:31 -0400

    animaciones agregadas

--- juegoPrueba/juegoPrueba.py
+++ juegoPrueba/juegoPrueba.py
@@ -8,9 +8,9 @@ def movimiento_enemigo(enemigos_list):
         for enemigos_rect in enemigos_list:
             enemigos_rect.x -= 5
             if enemigos_rect.bottom == 300:
-                screen.blit(slime,enemigos_rect)
+                screen.blit(slime_Surf,enemigos_rect)
             else:
-                screen.blit(fantasma,enemigos_rect)
+                screen.blit(fantasma_Surf,enemigos_rect)
         enemigos_list = [enemigo for enemigo in enemigos_list if enemigo.x > -16]
         return enemigos_list
     else:
@@ -21,7 +21,15 @@ def colisiones(player,enemigos_list):
              if player.colliderect(enemigos_rect):
                  return False
     return True
-
+def player_Animacion():
+    global player_Index
+    if player_Rect.bottom < 300:
+        return player_Jump
+    else:
+        player_Index += 0.1
+        if player_Index > len(player_Run):
+            player_Index = 0
+        return player_Run[int(player_Index)]
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("Proyecto")
 reloj = pygame.time.Clock()
@@ -33,14 +41,32 @@ font = pygame.font.Font("font/NotJamSlab14.ttf",50)
 texto = font.render("le bomb",False,"cornsilk")
 textoOver = font.render("GAME OVER",False,"white")
 texto_Rect = texto.get_rect(center = (300,50))
-fantasma = pygame.image.load("Characters/Enemies/sprites/Ghost/ghost1.png").convert_alpha()
-slime = pygame.image.load("Characters/Enemies/sprites/Slime/slime3.png").convert_alpha()
+fantasma_1 = pygame.image.load("Characters/Enemies/sprites/Ghost/ghost1.png").convert_alpha()
+fantasma_2 = pygame.image.load("Characters/Enemies/sprites/Ghost/ghost2.png").convert_alpha()
+fantasma = [fantasma_1,fantasma_2]
+fantasma_Index = 0
+fantasma_Surf = fantasma[fantasma_Index]
+slime_1 = pygame.image.load("Characters/Enemies/sprites/Slime/slime1.png").convert_alpha()
+slime_2 = pygame.image.load("Characters/Enemies/sprites/Slime/slime2.png").convert_alpha()
+slime = [slime_1,slime_2]
+slime_Index = 0
+slime_Surf = slime[slime_Index]
 enemigos_rect_list = []
-player = pygame.image.load("Characters/Player/sprites/Player-run/player-run1.png").convert_alpha()
-player_Rect = player.get_rect(bottomright = (80,300))
+player_Run1 = pygame.image.load("Characters/Player/sprites/Player-run/player-run1.png").convert_alpha()
+player_Run2 = pygame.image.load("Characters/Player/sprites/Player-run/player-run2.png").convert_alpha()
+player_Run3 = pygame.image.load("Characters/Player/sprites/Player-run/player-run3.png").convert_alpha()
+player_Index = 0
+player_Run = [player_Run1,player_Run2,player_Run3]
+player_Jump = pygame.image.load("Characters/Player/sprites/Player-jump/player-jump1.png").convert_alpha()
+player_Surf = player_Run[player_Index]
+player_Rect = player_Surf.get_rect(bottomright = (80,300))
 gravedad = 0
 timer_enemigos = pygame.USEREVENT + 1
-pygame.time.set_timer(timer_enemigos,1400)
+pygame.time.set_timer(timer_enemigos,1000)
+timer_Fantasma = pygame.USEREVENT + 2
+pygame.time.set_timer(timer_Fantasma,420)
+timer_Slime = pygame.USEREVENT + 3
+pygame.time.set_timer(timer_Slime,500)
 while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
@@ -52,9 +78,21 @@ while True:
                     gravedad = -15
             if evento.type == timer_enemigos:
                 if randint(0,1):
-                    enemigos_rect_list.append(fantasma.get_rect(midbottom = (900,250)))
+                    enemigos_rect_list.append(fantasma_1.get_rect(midbottom = (900,250)))
                 else:
-                    enemigos_rect_list.append(slime.get_rect(midbottom = (900,300)))
+                    enemigos_rect_list.append(slime_1.get_rect(midbottom = (900,300)))
+            if evento.type == timer_Fantasma:
+                if fantasma_Index == 0:
+                    fantasma_Index = 1
+                else:
+                    fantasma_Index = 0
+                fantasma_Surf = fantasma[fantasma_Index]
+            if evento.type == timer_Slime:
+                if slime_Index == 0:
+                    slime_Index = 1
+                else:
+                    slime_Index = 0
+                slime_Surf = slime[slime_Index]
         else:
             if evento.type == pygame.KEYDOWN and evento.key == pygame.K_SPACE:
                 game_Active = True
@@ -68,7 +106,8 @@ while True:
         enemigos_rect_list = movimiento_enemigo(enemigos_rect_list)
         if player_Rect.bottom >= 300:
             player_Rect.bottom = 300
-        screen.blit(player,player_Rect)
+        player_Surf_Actual = player_Animacion()
+        screen.blit(player_Surf_Actual,player_Rect)
         game_Active = colisiones(player_Rect,enemigos_rect_list)
     else:
         screen.fill("cyan4")
